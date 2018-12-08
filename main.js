790165d4f41df57809f11fff262d782fb6c7df53
shingo <reg@shin-go.net>
2018-12-09 01:41:28 +0900

    設定ダイアログの実装

--- main.js
+++ main.js
@@ -6,9 +6,30 @@ const application = require("application");
 const fileNameSuffix = '_minify';
 const nodeNameSuffix = '_Image_minify';
 
+let dialog;
+
+const dialogLabels = {
+  default: {
+    setting:'Setting',
+    scale:'Scale (0.1 - 5)',
+    quality:'Jpg Quality (1 - 100)',
+    rerun:'Rerun Minify',
+    cancel:'Cancel',
+    save:'Save'
+  },
+  ja: {
+    setting:'設定',
+    scale:'画像サイズの比率 (0.5 - 5)',
+    quality:'Jpg画質 (1 - 100)',
+    rerun:'画像を再縮小を許可する',
+    cancel:'キャンセル',
+    save:'保存'
+  }
+}
+
 let setting = {
-  quality: 80,
   scale: 1,
+  quality: 80,
   rerun:false
 }
 
@@ -112,10 +133,65 @@ function checkIsCompressed(node){
   }
 }
 
-function settingCommand(selection) {
+async function settingCommand(selection) {
     console.log("setting is running!");
+    var result = await createDialog(setting).showModal();
+    setting = result;
+    console.log('Settting changed:',setting);
+
 }
 
+function createDialog(setting){
+
+  let saveButton, cancelButton, qualityInput, scaleInput, rerunInput;
+  const labels = dialogLabels.ja;
+
+  // Create Html Element
+  if(!dialog){
+    dialog = document.createElement("dialog");
+    var html = '<style>form {width: 240px;}.h1 {align-items: center;justify-content: space-between;display: flex;flex-direction: row;}.icon {border-radius: 4px;width: 24px;height: 24px;overflow: hidden;}</style>';
+    html += '<form method="dialog">';
+    html += '<h1 class="h1"><span>' + labels.setting + '</span><img class="icon" src="./assets/icon.png" /></h1><hr />';
+    html += '<label><span>' + labels.scale + '</span>';
+    html += '<input type="number" min="0.1" max="5" value="1" id="scaleInput" /></label>';
+    html += '<label><span>' + labels.quality + '</span>';
+    html += '<input type="number" min="1" max="100" value="80" id="qualityInput" /></label>';
+    html += '<label class="row"><input type="checkbox" id="rerunInput" /><span>' + labels.rerun + '</span></label>';
+    html += '<footer><button uxp-variant="primary" id="cancelButton">' + labels.cancel + '</button><button type="submit" uxp-variant="cta" id="saveButton">' + labels.save + '</button></footer></form>';
+
+    dialog.innerHTML = html;
+    document.body.appendChild(dialog);
+
+  }
+
+  // Define Element
+  saveButton  = document.getElementById("saveButton");
+  cancelButton = document.getElementById("cancelButton");
+  scaleInput = document.getElementById("scaleInput");
+  qualityInput = document.getElementById("qualityInput");
+  rerunInput = document.getElementById("rerunInput");
+
+  // Event
+  saveButton.addEventListener('click', function(e){
+    dialog.close({
+      scale: scaleInput.value - 0,
+      quality: qualityInput.value - 0,
+      rerun: rerunInput.checked
+    });
+    e.preventDefault();
+  });
+  cancelButton.addEventListener('click', function(e){
+    dialog.close(false);
+    e.preventDefault();
+  });
+
+  // Set Default Value
+  scaleInput.value = setting.scale;
+  qualityInput.value = setting.quality;
+  rerunInput.checked = setting.rerun;
+
+  return dialog;
+}
 
 
 
