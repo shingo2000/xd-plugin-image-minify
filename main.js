217aea3800c7d5cd075b6f14be5016f30d347050
shingo <reg@shin-go.net>
2018-12-08 23:31:17 +0900

    menu項目を設定

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-/*
- * Sample plugin scaffolding for Adobe XD.
- *
- * Visit http://adobexdplatform.com/ for API docs and more sample code.
- */
+const {Rectangle, Color} = require("scenegraph");
 
-var {Rectangle, Color} = require("scenegraph");
+function minifyImagesCommand(selection) {
+    console.log("minifyImagesCommand is running!");
 
-function myPluginCommand(selection) {
-    // Go to Plugins > Development > Developer Console to see this log output
-    console.log("Plugin command is running!");
-
-    // Insert a red square at (0, 0) in the current artboard or group/container
-    var shape = new Rectangle();
-    shape.width = 100;
-    shape.height = 100;
-    shape.fill = new Color("#f00");
-    selection.insertionParent.addChild(shape);
 }
+
+
+function settingCommand(selection) {
+    console.log("setting is running!");
+
+}
+
+
 
 module.exports = {
     commands: {
-        myPluginCommand: myPluginCommand
+        minifyImages: minifyImagesCommand,
+        setting: settingCommand
     }
 };
